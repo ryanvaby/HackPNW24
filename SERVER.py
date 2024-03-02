571fdacca3ccb322e23e374c51b4916e92030785
Your Name <ryan.aby@outlook.com>
2024-03-02 12:17:43 -0800

    SERVER updates

--- SERVER.py
+++ SERVER.py
@@ -1,4 +1,16 @@
-import flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 import numpy
 import pandas
 import openai
+
+# Setup for flask application
+app = Flask(__name__)
+
+# Declaring the following main_func() method as the code to run when user requests the home page of site
+    
+@app.route("/", methods=["GET", "POST"])
+def main():
+    return render_template("index.html")
+
+def genImage():
+    
