55cbe09d4ed294ccdb25db561f5452e3d5ca226c
Your Name <ryan.aby@outlook.com>
2024-03-02 15:31:14 -0800

    SERVER updates

--- SERVER.py
+++ SERVER.py
@@ -1,50 +1,61 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-import numpy
-import pandas
 from openai import images, OpenAI
 
 # Setup for flask application
 app = Flask(__name__)
 
 # Declaring the following main_func() method as the code to run when user requests the home page of site
-    
+gloabl_prompt = ""
+
 @app.route("/", methods=["GET", "POST"])
 def main():
     if request.method == "POST":
-            roomHeight = request.form(roomHeight)
+            furniture_items = ["bedSelect", "tableSelect", "dresserSelect", "bookshelfSelect", "chairSelect" ]
+            for item in furniture_items:
+                if request.form.get(item) == 'on':
+                genImage("bed", request.form(bedLength), request.form(bedWidth))
+
+            dimensions = [roomLength, roomWidth, bedWidth, bedLength, tableLength, tableWidth, ]
+            roomLength = request.form(roomLength)
             roomWidth = request.form(roomWidth)
             bedWidth = request.form(bedWidth)
-            bedHeight = request.form(bedHeight)
+            bedLength = request.form(bedLength)
             tableWidth = request.form(tableWidth)
-            tableHeight = request.form(tableHeight)
+            tableLength = request.form(tableLength)
             dresserWidth = request.form(dresserWidth)
-            dresserHeight = request.form(dresserHeight)
+            dresserLength = request.form(dresserLength)
             bookshelfWidth = request.form(bookshelfWidth)
-            bookshelfHeight = request.form(bookshelfHeight)
+            bookshelfLength = request.form(bookshelfLength)
             chairWidth = request.form(chairWidth)
-            chairHeight = request.form(chairHeight)
+            chairLength = request.form(chairLength)
 
-            if not user:
-                print('User is required!')
+            if not roomLength:
+                roomLength = 0
+            if not roomWidth:
+                roomWidth = 0
+            if not bedLength:
+                bedLength = 0
+            if not bedWidth:
+                bedWidth = 0
+            if not tableLength:
+                tableLength = 0
+            if not tableWidth:
+                tableWidth = 0
+            if not dresserLength:
+                dresserLength = 0
+            if not dresserWidth:
+                dresserWidth = 0
+            if not bookshelfLength:
+                bookshelfLength = 0
+            if not bookshelfWidth:
+                bookshelfWidth = 0
+            if not chairLength:
+                chairLength = 0
+            if not chairWidth:
+                chairWidth = 0
 
-            # Determing whether the use wants to input their own domains or use the hard-coded ones
-            if user.lower() == "my own":
-                domains = request.form['domains']
-                if not domains:
-                    print('Domains is required!')
+            #genImage(roomLength, roomWidth, bedWidth, bedLength, tableLength, tableWidth, bookshelfLength, bookshelfWidth, chairLength, chairWidth, dresserLength,dresserWidth)
 
-                # Collecting the user's desired domains and running the measure_RTT function to store the average RTT of each domain
-                return redirect(url_for('output', target_domains=domains))
-            # Performing the same functions on pre-entered domains
-            elif user.lower() == "yours":
-                area = request.form['area']
-                if not area:
-                    print('Area is required!')
-
-                return redirect(url_for('output', target_domains=area))
-            else:
-                return render_template("home.html")
-        
-    return render_template("home.html")
-def genImage(bed, table, bookshelf, chair, dresser):
-    
+    else:
+        return render_template("index.html")
+        #def genImage(bed, table, bookshelf, chair, dresser):
